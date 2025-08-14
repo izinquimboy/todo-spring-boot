697deaf2d168d8677deec399993c6073f594813d
Iziel Inquimboy <izinquimboy@gmail.com>
2025-08-14 12:07:06 -0400

    added createTask function for database interaction from front end

--- src/main/java/com/app/todo/controller/TaskController.java
+++ src/main/java/com/app/todo/controller/TaskController.java
@@ -5,7 +5,8 @@ import com.app.todo.services.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,6 +18,19 @@ public class TaskController {
 
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTasks(Model model) {
+        List<Task> tasks = taskService.getALlTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
+
+    @PostMapping
+    public String createTask(@RequestParam String title) {
+        taskService.createTask(title);
+        return "redirect:/";
     }
 
     @GetMapping
--- src/main/java/com/app/todo/services/TaskService.java
+++ src/main/java/com/app/todo/services/TaskService.java
@@ -18,4 +18,11 @@ public class TaskService {
     public List<Task> getALlTasks() {
         return taskRepository.findAll();
     }
+
+    public void createTask(String title) {
+        Task task = new Task();
+        task.setTitle(title);
+        task.setCompleted(false);
+        taskRepository.save(task);
+    }
 }
