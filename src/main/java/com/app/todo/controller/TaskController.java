5ec052a7149143178c5e648ac5861e361dff0e46
Iziel Inquimboy <izinquimboy@gmail.com>
2025-08-14 10:19:56 -0400

    controller and service setup complete

--- src/main/java/com/app/todo/controller/TaskController.java
+++ src/main/java/com/app/todo/controller/TaskController.java
@@ -1,4 +1,28 @@
 package com.app.todo.controller;
 
+import com.app.todo.models.Task;
+import com.app.todo.services.TaskService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping("/tasks")
 public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public String getTasks(Model model) {
+        List<Task> tasks = taskService.getALlTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
 }
--- src/main/java/com/app/todo/services/TodoService.java
+++
@@ -1,4 +0,0 @@
-package com.app.todo.services;
-
-public class TodoService {
-}
